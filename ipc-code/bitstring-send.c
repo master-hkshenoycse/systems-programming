5017da331e4eb8c31a5581383835c9f849893bb6
hkshenoy <hkshenoy@hkshenoy.myguest.virtualbox.org>
2026-05-18 23:04:49 +0530

    ipc-code: Implement bitstring communication using POSIX signals
    - Added parent-child IPC using Unix signals
    - Implemented transmission of 8-bit binary strings via SIGUSR1 and SIGUSR2
    - Added signal handlers for receiving bits in child process
    - Added synchronization mechanism to ensure child readiness before transmission
    - Implemented bit reconstruction logic in child process
    - Added input validation for fixed-length binary strings
    - Used pause() and signal handlers for asynchronous communication
    - Added proper child termination and parent reaping using wait()
    - Demonstrates signal-based inter-process communication and synchronization

--- ipc-code/bitstring-send.c
+++ ipc-code/bitstring-send.c
@@ -1,82 +1,138 @@
-#include <stdio.h> // for printf
-#include <unistd.h> // for any linux utilities
-#include <signal.h> // for signal
-#include <sys/wait.h> // for wait() and fork
-#include <string.h> // for memset
-#include <stdlib.h> // for exit
-#include <sys/types.h> // for kill
-#include <stdbool.h> // for bool
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdbool.h>
 
-#define LENGTH 8 // The fixed length bitstring
+#define LENGTH 8
 
-char recvdString[9]; // the buffer for the child to store the received string into
-bool canSend = false; // synchronization mechanism for notifying the parent that the child is ready to receive
+char recvdString[9];
 
-void sigHandle1(int sig) { 
-    // TODO : Implement this function for when child receives a 1
+bool canSend = false;
+
+volatile sig_atomic_t recvIndex = 0;
+
+/* Signal handler for receiving bit 1 */
+void sigHandle1(int sig)
+{
+    recvdString[recvIndex++] = '1';
 }
 
-void sigHandle0(int sig) {
-    // TODO : Implement this function for when child receives a 0
+/* Signal handler for receiving bit 0 */
+void sigHandle0(int sig)
+{
+    recvdString[recvIndex++] = '0';
 }
 
-void synchronizeParent (int sig) { // helper function to ensure the parent only sends once the child is ready to receive
+/* Synchronization handler */
+void synchronizeParent(int sig)
+{
     canSend = true;
 }
 
-int main () {
+int main()
+{
+    signal(SIGUSR1, synchronizeParent);
 
-    signal(SIGUSR1,synchronizeParent); // to ensure parent is able to understand when child is telling it that its ready to receive
-    int cpid = fork(); // fork
+    int cpid = fork();
+
+    if (cpid < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    /* CHILD PROCESS */
     if (cpid == 0) {
-        /*
-        TODO: Implement signal handling mechanisms to catch 1 or 0 from parent
-        */
-        kill(getppid(),SIGUSR1); // Sends signal to parent that child is ready to receive
-        /*
-        TODO: Add some sort of logic for what you want to do after signal is received
-        */
-        recvdString[8] = '\0'; // Null terminates the string so that it prints in an expected manner
-        printf("[Child] Received bitstring is\t%s\n",recvdString); // Do not edit, prints the received bitstring
+
+        /* Register handlers */
+
+        signal(SIGUSR1, sigHandle1);
+
+        signal(SIGUSR2, sigHandle0);
+
+        /* Notify parent child is ready */
+        kill(getppid(), SIGUSR1);
+
+        /* Wait until all 8 bits received */
+        while (recvIndex < LENGTH) {
+            pause();
+        }
+
+        recvdString[8] = '\0';
+
+        printf("[Child] Received bitstring is\t%s\n",
+               recvdString);
 
         exit(0);
     }
+
+    /* PARENT PROCESS */
     else {
 
-        printf("Please input a %d-bit bitstring:\t",LENGTH);
+        printf("Please input a %d-bit bitstring:\t",
+               LENGTH);
 
-        char tmp[256]; // buffer to store bitstring
-        fgets(tmp,LENGTH + 1,stdin); // Take input from user
+        char tmp[256];
 
-        if (strlen(tmp) != 8) {
-            printf("Error : Input string not of length %d\n",LENGTH);
-            kill(cpid,9);
+        fgets(tmp, LENGTH + 1, stdin);
+
+        if (strlen(tmp) != LENGTH) {
+
+            printf("Error : Input string not of length %d\n",
+                   LENGTH);
+
+            kill(cpid, SIGKILL);
+
             wait(NULL);
+
             exit(1);
         }
 
-        for (int i=0; i < LENGTH; i++) {
+        for (int i = 0; i < LENGTH; i++) {
+
             if (!(tmp[i] == '1' || tmp[i] == '0')) {
-                printf("Error : Input string not a bitstring at index %d char %c\n",i,tmp[i]);
-                kill(cpid,9);
+
+                printf("Error : Input string not a bitstring at index %d char %c\n",
+                       i,
+                       tmp[i]);
+
+                kill(cpid, SIGKILL);
+
                 wait(NULL);
+
                 exit(1);
             }
         }
 
-        printf("[Parent] Input bitstring is \t%s\n",tmp);
+        printf("[Parent] Input bitstring is \t%s\n",
+               tmp);
 
-        while (!canSend) {;} // Wait until the child is ready to receive
-        for (int i=0; i < LENGTH; i++) {
+        /* Wait until child ready */
+        while (!canSend) {
+            ;
+        }
+
+        /* Send bits one by one */
+
+        for (int i = 0; i < LENGTH; i++) {
+
             if (tmp[i] == '1') {
-                // TODO : Add mechanism to send 1 to child
+
+                kill(cpid, SIGUSR1);
             }
             else {
-                // TODO : Add mechanism to send 0 to child
+
+                kill(cpid, SIGUSR2);
             }
-            sleep(1); // This is necessary to ensure the signals 1]all get sent to child and 2] are sent in correct ordering, increase time to increase reliability
+
+            sleep(1);
         }
-        wait(NULL); // reap the child
+
+        wait(NULL);
     }
 
+    return 0;
 }
